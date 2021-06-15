d63126694950099477e473126544a558d52540fa
EllieAzaveda <elliemiller66@icloud.com>
2021-06-15 09:50:16 -0600

    Add error handling to fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,14 @@
+export const checkForErr = (response) => {
+  if(response.status >= 500) {
+    return 'Uhoh! Something is wrong with our system. Please try back later.'
+  } else if (!response.ok) {
+    return 'Something went wrong. Please try again later.'
+  } else {
+    return response.json()
+  }
+}
+
 export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+  .then(checkForErr)
 }
