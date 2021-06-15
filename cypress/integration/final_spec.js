eb6ce19dfd43602280b505f38247d726c1f567af
EllieAzaveda <elliemiller66@icloud.com>
2021-06-15 11:26:25 -0600

    Remove example files

---
+++ cypress/integration/final_spec.js
@@ -0,0 +1,25 @@
+describe('Show main view of URL Shortener App', () => {
+
+  beforeEach(() => {
+    cy.fixture('mockData.json')
+      .then(mockData => {
+        cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
+          statusCode: 201,
+          delay: 100,
+          body: mockData
+        })
+      })
+    cy.visit('http://localhost:3000')
+  });
+
+  it('Should be able to visit the main page', () => {
+    cy.url().should('eq', 'http://localhost:3000/')
+  });
+
+  it('Should display the title upon page load', () => {
+    cy.contains('h1', 'URL Shortener')
+  });
+
+
+
+});
