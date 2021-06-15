3a5c8ed84f1cff8405e4f57c3455c773989888df
EllieAzaveda <elliemiller66@icloud.com>
2021-06-15 11:05:07 -0600

    Add post in handle submit

--- src/components/UrlForm/UrlForm.js
+++ src/components/UrlForm/UrlForm.js
@@ -5,22 +5,50 @@ class UrlForm extends Component {
     super();
     this.props = props;
     this.state = {
+      urls: props.urls,
       title: '',
-      urlToShorten: ''
+      urlToShorten: '',
+      shortenedURL: ''
     };
   }
 
-  handleNameChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+  shortenURL = (urlInput) => {
+    let urlArr = urlInput.split('.com/')
+    let baseURL = urlArr[0]
+    let shortyLength = Math.ceil(urlArr[1].length/3)
+    let shortyURL =  urlArr[1].slice(0, shortyLength);
+
+    return `${baseURL}.com/${shortyURL}`;
   }
 
-  handleSubmit = e => {
-    e.preventDefault();
-    this.clearInputs();
+  handleTitleChange = e => {
+    this.setState({ title: e.target.value });
+  }
+
+  handleURLChange = e => {
+    this.setState({ urlToShorten: e.target.value });
   }
 
   clearInputs = () => {
     this.setState({title: '', urlToShorten: ''});
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    return fetch(`http://localhost:3001/api/v1/urls`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        long_url: this.state.urlToShorten,
+        short: this.shortenURL(this.state.urlToShorten),
+        title: this.state.title,
+      })
+    })
+    this.clearInputs();
   }
 
   render() {
@@ -31,15 +59,15 @@ class UrlForm extends Component {
           placeholder='Title...'
           name='title'
           value={this.state.title}
-          onChange={e => this.handleNameChange(e)}
+          onChange={e => this.handleTitleChange(e)}
         />
 
         <input
           type='text'
           placeholder='URL to Shorten...'
-          name='title'
-          value={this.state.title}
-          onChange={e => this.handleNameChange(e)}
+          name='urlToShorten'
+          value={this.state.urlToShorten}
+          onChange={e => this.handleURLChange(e)}
         />
 
         <button onClick={e => this.handleSubmit(e)}>
